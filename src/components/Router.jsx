79ef57bc3c7e71438e46b9c92c135384c3f25624
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-11-27 09:54:57 -0600

    Initial commit

---
+++ src/components/Router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from '../App';
+
+export default function Router() {
+  const router = createBrowserRouter([{ path: '/', element: <App /> }]);
+
+  return <RouterProvider router={router} />;
+}
