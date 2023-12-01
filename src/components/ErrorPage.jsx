da72871613687d671c89d865d807b8283ec4929b
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-12-01 10:56:38 -0600

    Add Error page display

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,12 +5,14 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id='error-page'>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id='error-page' className='h-screen flex items-center justify-center'>
+      <div className='h-full flex flex-col items-center justify-center gap-4'>
+        <h1 className='font-bold text-3xl'>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p className='text-gray-600'>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
   );
 }
