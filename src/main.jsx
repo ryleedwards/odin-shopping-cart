325cfb10c3c528ed3963ab4567fdafc7bea327d2
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-12-06 11:00:46 -0600

    Change router import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Router from './components/Router.jsx';
 import './index.css';
+import Router from './router/Router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
