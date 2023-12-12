f88a215235cdf4a3678a7b5616267c6e6e06cd7e
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-12-12 10:59:55 -0600

    Add ShopPage route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import ErrorPage from '../pages/ErrorPage';
+import ShopPage from '../pages/ShopPage';
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <HomePage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/shop',
+      element: <ShopPage />,
       errorElement: <ErrorPage />,
     },
   ]);
