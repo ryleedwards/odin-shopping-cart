62beb45c6b00e774ccdfa82561a44e276c53b9cc
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-12-14 14:32:38 -0600

    Adjust height of main container

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -7,7 +7,7 @@ const ShopPage = () => {
   return (
     <>
       <Navbar />
-      <main className='h-screen bg-slate-200'>
+      <main className='bg-slate-200'>
         <div className='flex h-32 flex-col justify-center bg-gradient-to-br from-sky-950 to-blue-600 pb-5 text-slate-100'>
           <h2 className='px-4 py-2 text-3xl font-bold'>{bigText}</h2>
           <h3 className='px-8 text-lg'>{smallText}</h3>
