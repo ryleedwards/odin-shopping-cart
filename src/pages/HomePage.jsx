25323a2eb7c65665f7ff96ad611a04ad158bf686
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2023-12-15 11:19:02 -0600

    Lift items state and setQuantity func to App

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,21 @@
 import Navbar from '../components/Navbar';
 import slides from '../assets/slides';
 import ImageSlider from '../components/ImageSlider';
+import { array } from 'prop-types';
 
-const HomePage = () => {
+const HomePage = ({ items }) => {
   return (
     <>
-      <Navbar />
+      <Navbar items={items} />
       <main className='h-screen bg-slate-200'>
         <ImageSlider slides={slides} maxWidth={'xLarge'} height={'medium'} />
       </main>
     </>
   );
+};
+
+HomePage.propTypes = {
+  items: array.isRequired,
 };
 
 export default HomePage;
